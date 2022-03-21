fbc7269bf59e6e8a115bef2f6999ed61bd2251a0
눈눈이 <97512153+2nun@users.noreply.github.com>
2022-03-21 16:49:26 +0900

    【⛄】 03/21/22ㅣℍ𝕠𝕨 𝕥𝕠 𝕦𝕤𝕖 𝔹𝕠𝕠𝕝𝕖𝕒𝕟𝕤

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-//  variable 만드는 방법 2가지, "const, let"
-//  "const"는 상수, 생성 후 바꿀 수 없음
-//  "let" 생성할 때 사용, 생성 후에 값을 바꿀 수 있음. 재선언 X, 재할당 O
+//  Booleans 사용법★
+//  true, false, null, underfined 모두 타입이기 때문에,
+// "true", "false", "null" 이렇게 쓰면 좆됨.
 
-//  "variable" 은 태그 같은거라 보면 됨.
-//  var은 쓰지마 씨발
+const amIFat= null; // <= True or False
+let something; //  이게바로 undefined
 
-const a = 5; // a와 b는 바꾸지 않을거임
-const b = 2;
+console.log(something, amIFat);
 
-let myName = "2nun";
+//  우리가 기억할 것 : null 은 "절대로" 자연적으로 발생하지 않음.
+// null 은 우리가 Variable 안에 아무것도 없다는 것을 확실히 하기 위해 쓰는것
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello," + " 2nun");
-
-//===============================================================================
-
-myName = "nun2" //이렇게 내 이름을 "nun2"로 하면
-
-console.log("Your new name is " + myName); // "Your new name is nun2" 로 변경됨!!
-
-//  20line 위에서는 myName은 2nun이지만 20line 아래에서는 nun2.
+//  underfined와 null의 차이
+//  undefined: 변수는 선언했지만 값을 할당하지는 않음
+//  null: 변수에 null(값이 없다) 가 할당됨, 빈 방이라는 뜻
