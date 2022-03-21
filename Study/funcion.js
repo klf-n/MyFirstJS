83e81aa00046a4dcfb3c641e288942b2898bc1db
눈눈이 <97512153+2nun@users.noreply.github.com>
2022-03-21 21:05:52 +0900

    【⛄】 03/21/22ㅣ𝔽𝕦𝕟𝕔𝕚𝕠𝕟

---
+++ Study/funcion.js
@@ -0,0 +1,13 @@
+//  Function
+//  function은 계속 반복해서 사용할 수 있는 코드 조각임.
+
+function sayHello( ){
+    console.log("Hello!"); //  실행 할 코드
+};
+
+//  소괄호 안에 작성하는 것은 실행버튼을 누를 때마다 발생함.
+
+sayHello(); //  --> Hello!
+
+//  argument(인수)는 function을 실행하는 도안 정보를
+//  function에게 보낼 수 있는 방법으로, 소괄호 안에 위치함.
