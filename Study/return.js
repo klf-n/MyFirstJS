40a3218121ead6684561c9e1c701200900658f80
눈눈이 <97512153+2nun@users.noreply.github.com>
2022-03-22 18:05:22 +0900

    【⛄】 03/22/22ㅣℝ𝕖𝕥𝕦𝕣𝕟

---
+++ Study/return.js
@@ -0,0 +1,21 @@
+//  Return
+
+//  ★중★요★한★거★
+//  return 을 작동시키면 그 funcions값은 다시 못쓰게 됨. (return "까지만" 코드를 수행함.)
+
+const example = {
+
+    noDap: function(a,b){
+        return 1 + 1;
+        console.log("hello"); // return을 한 다음 console.log 를 써서 출력하지 않음.
+    }
+};
+
+const plusResult = calculator.plus(7, 2);
+
+console.log(plusResult);
+
+//  댓글 보다 웃겨서 여따 적음ㅋㅋ
+//  console.log는 그림의 떡. 꺼내 먹으려면 return필요..
+
+// ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 이거임 진짜ㅋㅋ
